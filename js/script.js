cb2a4fb9203f3bc9c58d5fcdcd776ece721906a4
xeromole <tqbrasil10@gmail.com>
2018-08-01 05:49:08 -0300

    Animacoes, jquery verificacoes, rodape, e about

--- js/script.js
+++ js/script.js
@@ -2,12 +2,24 @@ $(function() {
 
 
 	var toggleMenu = $(".portfolio__header__menu-toggle");
+	var toggleAnimation = $(".menu__toggle");
 	var toggleList = $(".portfolio__header__navigation");
 	var toggleheader = $(".portfolio__header");
+	var positionOfTop;
 	console.log(toggleMenu);
 	$(toggleMenu).on("click", function() {
 		$(toggleList).toggleClass("portfolio__header__navigation--active");
-		
+		$(toggleAnimation).toggleClass("menu-goggle__item--active");
 	});
+
+	$(window).scroll(function() {
+		positionOfTop = $(".portfolio__header").offset().top;
+		if(positionOfTop > 140) {
+			$(".portfolio__header").addClass("portfolio__header--scrolled");
+		}
+		else {
+			$(".portfolio__header").removeClass("portfolio__header--scrolled");
+		}
+	})
 
 });
