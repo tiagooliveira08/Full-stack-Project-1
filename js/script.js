5f044c1e10a54a65af13a99635bfca4c4de7916d
xeromole <tqbrasil10@gmail.com>
2018-08-01 06:00:52 -0300

    Script verificacao altura topo

--- js/script.js
+++ js/script.js
@@ -5,9 +5,16 @@ $(function() {
 	var toggleAnimation = $(".menu__toggle");
 	var toggleList = $(".portfolio__header__navigation");
 	var toggleheader = $(".portfolio__header");
+	var menuList = $(".portfolio__header__navigation");
+	var heightHeader;
+
+
+	console.log($(toggleheader).offset().top);
 	var positionOfTop;
 	console.log(toggleMenu);
 	$(toggleMenu).on("click", function() {
+		heightHeader = $(".portfolio__header").height();
+		$(menuList).css("top",+heightHeader);
 		$(toggleList).toggleClass("portfolio__header__navigation--active");
 		$(toggleAnimation).toggleClass("menu-goggle__item--active");
 	});
